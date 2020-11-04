224abaaa4fbcc23ac493b05907c5e1fecb796a61
weekeat-tan <tanwk_97@hotmail.com>
2020-11-04 09:34:28 +0800

    Finished the api calls for both getting company and plan recommendation

--- server.py
+++ server.py
@@ -3,14 +3,22 @@ from flask import request, jsonify, json
 
 import pandas as pd
 
-# from travel_insurance_company_recommender import data_preprocessing
-# from travel_insurance_company_recommender import train_data
+from travel_insurance_company_recommender import data_preprocessing
+from travel_insurance_company_recommender import train_data
 
 app = Flask(__name__)
 
 @app.route('/get_travel_insurance_company', methods=['GET', 'POST'])
 def get_travel_insurance_company():
+    # Use trained KNN to predict subsequent company.
+    json_data = request.get_json()
 
+    # df = pd.read_csv("project_data.csv")
+    incoming_data = pd.DataFrame(json_data, index=[0])
+    incoming_data = df.append(incoming_data, ignore_index=True)
+
+    X_test = incoming_data.copy().drop(['ins_comp', 'subs_plan'], axis=1)
+    X_test = data_preprocessing(X_test)
 
     recommended_company = knn_for_company.predict(X_test[-1:])[0]
     ranking = knn_for_company.predict_proba(X_test[-1:])[0]
@@ -34,7 +42,19 @@ def get_travel_insurance_company():
 
 @app.route('/get_travel_insurance_plan', methods=['GET', 'POST'])
 def get_travel_insurance_plan():
+    # Use trained KNN to predict subsequent plan.
+    json_data = request.get_json()
 
+    print(json_data)
+
+    incoming_data = pd.DataFrame(json_data, index=[0])
+    incoming_data = df.append(incoming_data, ignore_index=True)
+
+    X_test = incoming_data.copy().drop('subs_plan', axis=1)
+    X_test = data_preprocessing(X_test)
+
+    print("after preprocessing")
+    print(X_test)
 
     ranking = knn_for_plan.predict_proba(X_test[-1:])[0]
 
